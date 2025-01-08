e92fa66909bb6f4d4da06f7b9900991e7034f315
tanishq0917t <tanishqrawat8@gmail.com>
2025-01-08 09:40:03 +0530

    Re-configuring size limit

--- tests/rotation_test.py
+++ tests/rotation_test.py
@@ -3,7 +3,7 @@ from loggingontips.LogLevels import LogLevel
 l1=Logger()
 
 def run():
-    for i in range(100000):
+    for i in range(1000000):
         l1.log(LogLevel.DEBUG,'Working?')
 
 if __name__=="__main__":
